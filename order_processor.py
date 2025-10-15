217802616ff126c09c9fc818c8a74c8a48f47019
DathwikK <dk649@njit.edu>
2025-10-15 19:16:22 -0400

    updated with code that creates customers.json

--- order_processor.py
+++ order_processor.py
@@ -10,3 +10,13 @@ input_file = sys.argv[1]
 # Load orders data
 with open(input_file, "r") as f:
     orders = json.load(f)
+
+# Create customers.json
+customers = {}
+for order in orders:
+    phone = order["phone"]
+    name = order["name"].strip()
+    customers[phone] = name
+
+with open("customers.json", "w") as f:
+    json.dump(customers, f, indent=4)
